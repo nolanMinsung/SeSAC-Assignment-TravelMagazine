ea6178b66aa400a92172cc4cbe8292e85077fb04
Minsung Kim <mskim4048@naver.com>
2025-07-13 10:32:51 +0900

    Initial Commit

---
+++ SeSAC-Assignment-TravelMagazine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-Assignment-TravelMagazine
+//
+//  Created by 김민성 on 7/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SeSAC-Assignment-TravelMagazineUITests/SeSAC_Assignment_TravelMagazineUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SeSAC_Assignment_TravelMagazineUITestsLaunchTests.swift
+//  SeSAC-Assignment-TravelMagazineUITests
+//
+//  Created by 김민성 on 7/13/25.
+//
+
+import XCTest
+
+final class SeSAC_Assignment_TravelMagazineUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
