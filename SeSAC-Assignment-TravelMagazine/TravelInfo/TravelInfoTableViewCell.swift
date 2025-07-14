ade84c9c6ec1b50fc1409744047ed9ac9030037a
Minsung Kim <mskim4048@naver.com>
2025-07-15 00:27:38 +0900

    design: TravelInfoTableViewCell UI 구현 및 Outlet 설정

--- SeSAC-Assignment-TravelMagazine/TravelInfo/TravelInfoTableViewCell.swift
+++ SeSAC-Assignment-TravelMagazine/TravelInfo/TravelInfoTableViewCell.swift
@@ -8,16 +8,21 @@
 import UIKit
 
 class TravelInfoTableViewCell: UITableViewCell {
-
+    
+    
+    @IBOutlet var placeNameLabel: UILabel!
+    @IBOutlet var placeDescriptionLabel: UILabel!
+    @IBOutlet var reviewInfoLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        
     }
     
 }
