c82ec5c963af7ea77ebc435493f33724fd663daf
Minsung Kim <mskim4048@naver.com>
2025-07-15 08:15:09 +0900

    add: TravelInfo 테이블뷰에서 사용할 cell xib 및 파일 추가

---
+++ SeSAC-Assignment-TravelMagazine/TravelInfo/TravelInfoTableAdCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelInfoTableAdCell.swift
+//  SeSAC-Assignment-TravelMagazine
+//
+//  Created by 김민성 on 7/15/25.
+//
+
+import UIKit
+
+class TravelInfoTableAdCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- SeSAC-Assignment-TravelMagazine/TravelInfo/TravelInfoTableViewCell.swift
+++ SeSAC-Assignment-TravelMagazine/TravelInfo/TravelInfoTableViewCell.swift
@@ -7,22 +7,39 @@
 
 import UIKit
 
+import Kingfisher
+
 class TravelInfoTableViewCell: UITableViewCell {
-    
     
     @IBOutlet var placeNameLabel: UILabel!
     @IBOutlet var placeDescriptionLabel: UILabel!
-    @IBOutlet var reviewInfoLabel: UILabel!
+    @IBOutlet var saveInfoLabel: UILabel!
+    
+    @IBOutlet var placeImageView: UIImageView!
+    @IBOutlet var likeButton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        placeImageView.layer.cornerRadius = 10
     }
     
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
+    override func prepareForReuse() {
+        super.prepareForReuse()
         
+        placeImageView.image = nil
+    }
+    
+    func configure(with item: Travel) {
+        placeNameLabel.text = item.title
+        placeDescriptionLabel.text = item.description
+        saveInfoLabel.text = "(1,812) • 저장 \(item.save ?? 0)"
+        
+        placeImageView.kf.setImage(
+            with: item.imageURL,
+            options: [.processor(DownsamplingImageProcessor(size: placeImageView.frame.size)),
+                      .scaleFactor(UIScreen.main.scale)]
+        )
     }
     
 }
