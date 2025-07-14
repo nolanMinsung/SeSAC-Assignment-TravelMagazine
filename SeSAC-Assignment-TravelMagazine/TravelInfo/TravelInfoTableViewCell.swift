9088a5b25e8160688687166dcc15a407cf3e7108
Minsung Kim <mskim4048@naver.com>
2025-07-14 22:36:14 +0900

    add: TravelInfo UI 관련 파일들 추가

---
+++ SeSAC-Assignment-TravelMagazine/TravelInfo/TravelInfoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelInfoTableViewCell.swift
+//  SeSAC-Assignment-TravelMagazine
+//
+//  Created by 김민성 on 7/14/25.
+//
+
+import UIKit
+
+class TravelInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
