488e499da58faa2442d03f9f9870f045a651d3c1
Minsung Kim <mskim4048@naver.com>
2025-07-15 16:33:51 +0900

    TravelADViewController 스토리보드 및 파일 생성 후 연결

---
+++ SeSAC-Assignment-TravelMagazine/TravelInfo/TravelPlaceDetail/TravelADViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TravelADViewController.swift
+//  SeSAC-Assignment-TravelMagazine
+//
+//  Created by 김민성 on 7/15/25.
+//
+
+import UIKit
+
+class TravelADViewController: UIViewController {
+    
+    @IBOutlet var adTextLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func dismissButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+}
