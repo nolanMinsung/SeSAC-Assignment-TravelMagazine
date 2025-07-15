1aec628f9cda19c6e211b619c745579fe8c439be
Minsung Kim <mskim4048@naver.com>
2025-07-15 16:05:11 +0900

    TravelPlaceDetailViewController 스토리보드 및 파일 생성 후 연결

---
+++ SeSAC-Assignment-TravelMagazine/TravelInfo/TravelPlace/TravelPlaceDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TravelPlaceDetailViewController.swift
+//  SeSAC-Assignment-TravelMagazine
+//
+//  Created by 김민성 on 7/15/25.
+//
+
+import UIKit
+
+class TravelPlaceDetailViewController: UIViewController {
+    
+    @IBOutlet var travelImageView: UIImageView!
+    @IBOutlet var placeNameLabel: UILabel!
+    @IBOutlet var placeDescriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func popButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+}
