2ce3fff8513b03086962890ff45b9fc154bfc35d
Minsung Kim <mskim4048@naver.com>
2025-07-16 15:33:45 +0900

    feat: 도시 검색 뷰컨트롤러 기능 구현

--- CitySearchingApp/CitySearchingViewController.swift
+++ CitySearchingApp/CitySearchingViewController.swift
@@ -15,19 +15,78 @@ class CitySearchingViewController: UIViewController {
     
     @IBOutlet var citySearchBar: UISearchBar!
     @IBOutlet var cityFilteringSegmentedControl: UISegmentedControl!
-    
     @IBOutlet var tableView: UITableView!
+    
+    private var currentSearchText: String = ""
+    private var currentFilteringOption: CityFilteringOption = .all
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        citySearchBar.delegate = self
         
+        // table view settings
+        let nib = UINib(nibName: "CitySearchingTableViewCell", bundle: .main)
+        tableView.register(nib, forCellReuseIdentifier: "CitySearchingTableViewCell")
+        tableView.rowHeight = 150
+        
+        // initial data loading
+        setupDiffableDataSource()
+        reloadDiffableDataSource()
     }
     
+    private func setupDiffableDataSource() {
+        let cellProvider: UITableViewDiffableDataSource<Int, City>.CellProvider
+        = { tableView, indexPath, itemIdentifier in
+            
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: "CitySearchingTableViewCell"
+            ) as! CitySearchingTableViewCell
+            cell.configure(with: itemIdentifier)
+            return cell
+        }
+        
+        self.dataSource = CitySearchingDiffableDataSource(
+            tableView: tableView,
+            cellProvider: cellProvider
+        )
+        self.tableView.delegate = self
+    }
+    
+    private func reloadDiffableDataSource() {
+        dataSource.applySnapshot(
+            searchText: currentSearchText,
+            filteringOption: currentFilteringOption
+        )
+    }
     
     @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
-        
+        switch sender.selectedSegmentIndex {
+        case 0: currentFilteringOption = .all
+        case 1: currentFilteringOption = .domesticOnly
+        case 2: currentFilteringOption = .foreignOnly
+        default: fatalError("segmented control index out of range.")
+        }
+        reloadDiffableDataSource()
     }
     
+}
+
+
+// MARK: - UISearchBarDelegate
+extension CitySearchingViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        currentSearchText = searchText
+        reloadDiffableDataSource()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+}
+
+extension CitySearchingViewController: UITableViewDelegate {
     
 }
