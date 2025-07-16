ba7f8ebbf766d158f8dc562bbad127b6abb8fb48
Minsung Kim <mskim4048@naver.com>
2025-07-16 15:13:00 +0900

    add: 도시 검색 뷰컨트롤러 생성
    UIViewController + table view 이용하는 방법

---
+++ CitySearchingApp/CitySearchingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CitySearchingViewController.swift
+//  CitySearchingApp
+//
+//  Created by 김민성 on 7/16/25.
+//
+
+import UIKit
+
+class CitySearchingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
