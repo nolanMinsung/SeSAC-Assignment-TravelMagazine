ec78f5fd297a0c2708e8551984b819d01d8881d2
Minsung Kim <mskim4048@naver.com>
2025-07-16 16:30:08 +0900

    add: CityDetailViewController 스토리보드 및 파일 생성 후 IBOutlet, IBAction 연결

---
+++ CitySearchingApp/CityDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CityDetailViewController.swift
+//  CitySearchingApp
+//
+//  Created by 김민성 on 7/16/25.
+//
+
+import UIKit
+
+class CityDetailViewController: UIViewController {
+    
+    @IBOutlet var cityImageView: UIImageView!
+    
+    @IBOutlet var cityNameLabelKorean: UILabel!
+    @IBOutlet var cityNameLabelEnglish: UILabel!
+    
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
