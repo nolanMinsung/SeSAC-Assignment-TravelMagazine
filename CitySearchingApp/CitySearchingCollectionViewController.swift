6eeeead9d538b3f90a9a42f4d658a7d18b1dc6b0
Minsung Kim <mskim4048@naver.com>
2025-07-17 14:50:18 +0900

    add: 도시 검색 컬렉션 뷰 관련 스토리보드 및 파일 추가 후 IBOutlet, IBAction 연결

---
+++ CitySearchingApp/CitySearchingCollectionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CitySearchingCollectionViewController.swift
+//  CitySearchingApp
+//
+//  Created by 김민성 on 7/17/25.
+//
+
+import UIKit
+
+class CitySearchingCollectionViewController: UIViewController {
+    
+    
+    @IBOutlet var citySearchBar: UISearchBar!
+    @IBOutlet var cityFilteringSegmentedControl: UISegmentedControl!
+    @IBOutlet var cityListCollectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    
+    @IBAction func cityFilteringSegmentedControlValueChanged(_ sender: UISegmentedControl) {
+        
+        
+        
+    }
+    
+    
+}
---
+++ CitySearchingApp/cell/CitySearchingCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CitySearchingCollectionViewCell.swift
+//  CitySearchingApp
+//
+//  Created by 김민성 on 7/17/25.
+//
+
+import UIKit
+
+class CitySearchingCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var placeImageView: UIImageView!
+    @IBOutlet var placeNameLabel: UILabel!
+    @IBOutlet var placeDescriptionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+    
+    
+    
+}
